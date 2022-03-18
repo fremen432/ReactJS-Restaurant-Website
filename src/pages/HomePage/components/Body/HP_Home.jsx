a0f2a55575ad6eb103d79df5dc436d6ce3daddd4
fremen432 <cbmiller4897@gmail.com>
2022-03-18 02:41:32 -0500

    fixed hp home image fade

--- src/pages/HomePage/components/Body/HP_Home.jsx
+++ src/pages/HomePage/components/Body/HP_Home.jsx
@@ -2,39 +2,47 @@ import { hours, locations } from '../../../../Data'
 
 export default function Home() {
   return (
-    <div id="Home" className="Home snap-element">
-      <h1 className="restaurant_name" >SIMPLY PHO HOUSE</h1>
-      <h2>Central Texas Vietnamese Food & Asian Fusion</h2>
-      <div className="description" >
+    <>
+      <div className="home_hero_parent">
+        <div className=" hero-stack home_hero_layer1_image"></div>
+        <div className=" hero-stack home_hero_layer2_fade"></div>
 
-        <div className="locations" >
-          <h2 className="text-center" >Locations</h2>
+        <div id="Home" className=" hero-stack home_hero_layer3_textContent snap-element">
+          <h1 className="restaurant_name" >SIMPLY PHO HOUSE</h1>
+          <h2>Central Texas Vietnamese Food & Asian Fusion</h2>
+          <div className="description" >
 
-          {locations.map(el => 
-            <div className="grid grid-cols-5 ">
-              <span className="col-span-1">{el.name + ': '}</span>
-              <span className="col-span-3">{el.address}</span>
-              <a className="col-span-1" href="tel:+5555555555">{el.phone}</a>
+            <div className="locations" >
+              <h2 className="text-center" >Locations</h2>
+
+              {locations.map(el => 
+                <div className="grid grid-cols-5 ">
+                  <span className="col-span-1">{el.name + ': '}</span>
+                  <span className="col-span-3">{el.address}</span>
+                  <a className="col-span-1" href="tel:+5555555555">{el.phone}</a>
+                </div>
+                )}
             </div>
-            )}
-        </div>
 
-        <div className="hours" >
-          <h2 className="text-center" >Hours</h2>
-          <div className="hours_wrapper mx-40" >
-            {hours.map(el =>
-            <div className="grid grid-cols-2 " >
-              <span>{el.day}</span>
-              <div>
-                <span>{el.opening + ' - '}</span>
-                <span>{el.closing}</span>
+            <div className="hours" >
+              <h2 className="text-center" >Hours</h2>
+              <div className="hours_wrapper mx-40" >
+                {hours.map(el =>
+                <div className="grid grid-cols-2 " >
+                  <span>{el.day}</span>
+                  <div>
+                    <span>{el.opening + ' - '}</span>
+                    <span>{el.closing}</span>
+                  </div>
+                </div>
+                )}
               </div>
             </div>
-            )}
+
           </div>
         </div>
 
       </div>
-    </div>
+    </>
   )
 }
