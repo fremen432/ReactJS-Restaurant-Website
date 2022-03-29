5464559deaf345ebe0856d9a0ced3230a15b77e4
fremen432 <cbmiller4897@gmail.com>
2022-03-29 17:32:01 -0500

    fixed footer

--- src/misc-componenets/Footer.jsx
+++ src/misc-componenets/Footer.jsx
@@ -1,14 +1,10 @@
 export default function Footer() {
   return (
-    <div className="Footer">
-        <div>
-            <div className="grid grid-cols-4 ">
-                <a href='/#Home' className="block" >Home</a>
-                <a href='/#Menu' className="block" >Menu</a>
-                <a href='/order' className="block" >Order</a>
-                <a href='/#Contact' className="block" >Contact Us</a>
-            </div>
-        </div>
+    <div className="Footer grid grid-cols-4 ">
+        <a href='/#Home' className="footer-element" >Home</a>
+        <a href='/#Menu' className="footer-element" >Menu</a>
+        <a href='/order' className="footer-element" >Order</a>
+        <a href='/#Contact' className="footer-element" >Contact Us</a>
     </div>
   )
 }
--- src/misc-componenets/Menu.jsx
+++ src/misc-componenets/Menu.jsx
@@ -6,20 +6,17 @@ function Dish(props) {
     const dish_name_classes = 'grid grid-cols-4 '
 
     return (
-      <>
-        {/* <div className={"menu_item m-2 box p-2 pt-1 rounded-lg  " + (page =='HomePage' ? 'block ' : isHidden)}> */}
-        <div className={"menu_item m-2 p-2 pt-1 rounded-lg  " }>
-          <div className={dish_name_classes + " dish_name"} >
-              <h3 className="text-lg col-start-1 col-span-3 ">{dish.name}</h3>
-              <span id="price" className="text-sm col-start-4 col-span-1 text-center">{
-              (dish.price !== '' ? '$' + dish.price : '')
-              }</span>
-          </div>
-          <p className="text-sm">{dish.description}</p>
-          {(dish.price == '' || page == 'HomePage' ? '' : <button onClick={() => onAdd(dish)} className="order_btn rounded-md px-2 py-1 mt-1 text-sm">Add to order</button>
-          )}
+      <div className={"menu_item m-2 p-2 pt-1 rounded-lg  " }>
+        <div className={dish_name_classes + " dish_name"} >
+            <h3 className="text-lg col-start-1 col-span-3 ">{dish.name}</h3>
+            <span id="price" className="text-sm col-start-4 col-span-1 text-center">{
+            (dish.price !== '' ? '$' + dish.price : '')
+            }</span>
         </div>
-      </>
+        <p className="text-sm">{dish.description}</p>
+        {(dish.price == '' || page == 'HomePage' ? '' : <button onClick={() => onAdd(dish)} className="order_btn rounded-md px-2 py-1 mt-1 text-sm">Add to order</button>
+        )}
+      </div>
     );
 }
 
@@ -32,23 +29,25 @@ function DishCategory(props) {
       isHidden == true ? false :
       isHidden == false ? true :
       true
-      )
+    )
   }
 
   return(
-    <div className={"box m-2 shadow-lg box rounded-lg "}>
-    <h2 onClick={page =='HomePage' ? '' : toggleHidden} className={"OP_Menu_Title box text-center rounded-lg " + (page =='HomePage' ? '' : 'cursor-pointer ')}>{categoryTitle}</h2>
-    <div className={"OP_Menu_Dishes box " + (
-      page == 'HomePage' ? 'block' :
+    <div className={"m-2 shadow-lg rounded-lg "}>
+      <h2 className={"Menu_Title text-center rounded-lg " + (page =='HomePage' ? '' : 'cursor-pointer ')} onClick={page =='HomePage' ? '' : toggleHidden} >{categoryTitle}</h2>
+      <div className={"OP_Menu_Dishes " + (
+        page == 'HomePage' ? 'block' :
 
-      isHidden == true ? 'roll_DOWN_animation ':
-      isHidden == false ? 'roll_UP_animation ':
-      'hidden'
-    )}>
-      {dishes.map(dish =>
-        <Dish key={dish.id} dish={dish} onAdd={onAdd} page={page}/>
-      )}
-    </div>
+        isHidden == true ? 'roll_DOWN_animation ':
+        isHidden == false ? 'roll_UP_animation ':
+        'hidden'
+      )}>
+        {dishes.map( dish =>
+
+          <Dish key={dish.id} dish={dish} onAdd={onAdd} page={page}/>
+          
+        )}
+      </div>
   </div>
   )
 }
@@ -74,8 +73,10 @@ export default function Menu(props) {
       ">
 
         {/* Returns h1 with all category and respective menu items inside */}
-        {sortedMenu.map(arr =>
-          <DishCategory categoryTitle={arr[0].category} dishes={arr} onAdd={onAdd} page={page}/>
+        {sortedMenu.map( arr => 
+
+          <DishCategory categoryTitle={arr[0].category} dishes={arr} onAdd={onAdd} page={page}/> 
+
         )}
 
       </div>
