0da5d39d29395f7f77d546c132ad2aeec10e0135
fremen432 <cbmiller4897@gmail.com>
2022-05-09 10:57:13 -0500

    un-added tailwind config from .gitignore

--- src/components/HomePage/HP_Body.jsx
+++ src/components/HomePage/HP_Body.jsx
@@ -1,13 +1,13 @@
-import Home from './HP_Home'
-import Contact from './HP_Contact'
-import Menu from '../Menu'
+import Home from "./HP_Home";
+import Contact from "./HP_Contact";
+import Menu from "../Menu";
 
 export default function Body() {
-  return (
-    <div id="SECTION__Body" className="Body snap-type-y">
-        <Home/>
-        <Menu page='HomePage'/>
-        <Contact/>
-    </div>
-  )
+	return (
+		<div id="SECTION__Body" className="Body snap-type-y">
+			<Home />
+			<Menu page="HomePage" />
+			<Contact />
+		</div>
+	);
 }
